12796380a72c1f727ed13365a9bc6f209d755b42
abhinavoffice <abhinav.agarwal@wynk.in>
2019-03-27 18:58:51 +0530

    commit all

---
+++ src/main/java/com/airtel/android/auomation/airtelTVAndroid/App.java
@@ -0,0 +1,13 @@
+package com.airtel.android.auomation.airtelTVAndroid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
